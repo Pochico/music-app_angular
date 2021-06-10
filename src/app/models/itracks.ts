f0c37a294b3eb91be6bd2da3a9efe791a68d2844
Julen Castillo <julencas.desarrollo@gmail.com>
2021-06-10 20:48:18 +0200

    search song list router added

--- src/app/models/itracks.ts
+++ src/app/models/itracks.ts
@@ -1,11 +1,7 @@
 import { Ihits } from './ihits';
-import { Iartists } from './iartists';
-import { Itrack } from './itrack';
 
 export interface Itracks {
     tracks?: {
-        // hits: [{track: Itrack}],
-        // artists: Iartists
         hits: Ihits[],
     }
 }
